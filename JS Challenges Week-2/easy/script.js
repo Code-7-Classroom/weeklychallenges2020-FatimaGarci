4a07cc760371704e6f580f6e98270122a7be1e24
Fatima Rossmery Lopez Garcia <rossmery736garcia@gmail.com>
2020-03-26 17:44:44 -0400

    JS Challenges Week-2

---
+++ JS Challenges Week-2/easy/script.js
@@ -0,0 +1,7 @@
+var studentFirstName = ['Sam', 'Carly', 'Freddy'];
+var studentLastName = ['Puckett', 'Shay', 'Benson'];
+var studentAge = [15, 18, 17];
+
+console.log('Hello, my name is '+ studentFirstName[0]+ ' ' + studentLastName[0] + " I'm " + studentAge[0] + ' years old.');
+console.log('Hello, my name is '+ studentFirstName[1]+ ' ' + studentLastName[1] + " I'm " + studentAge[1] + ' years old.');
+console.log('Hello, my name is '+ studentFirstName[2]+ ' ' + studentLastName[2] + " I'm " + studentAge[2] + ' years old.');
