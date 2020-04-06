ebf1fe3231a4366343aa381f91904c24250ce64f
Fatima Rossmery Lopez Garcia <rossmery736garcia@gmail.com>
2020-04-06 16:11:37 -0400

    JS week3clear

--- JS-Challenges-Week3/medium/script.js
+++ JS-Challenges-Week3/medium/script.js
@@ -1,10 +1,18 @@
-var sharePizza = cutPizzaSlices(8);
 
-function cutPizzaSlices(slice){
- return function(sharePizza){
-     return "Each person gets " + (slice/sharePizza).toFixed(2) + " slices of pizza";
- }
-}
+/* IIFE - Immediately Invoked Function Expression 
+    If you wrap all of your code inside an IFFE, no global variables are exposed to the JavaScript namespace.
 
-console.log(sharePizza(2)); // prints "Each person gets 4.00 slices of pizza"
-console.log(sharePizza(3));// prints "Each person gets 2.67 slices of pizza"
+*/
+
+(function() {  
+    var sharePizza = cutPizzaSlices(8);
+
+    function cutPizzaSlices(slice){
+     return function(sharePizza){
+         return "Each person gets " + (slice/sharePizza).toFixed(2) + " slices of pizza";
+     }
+    }
+
+    console.log(sharePizza(2)); // prints "Each person gets 4.00 slices of pizza"
+    console.log(sharePizza(3));// prints "Each person gets 2.67 slices of pizza"
+})();
