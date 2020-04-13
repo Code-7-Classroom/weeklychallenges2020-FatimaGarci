3aaf0ca459ece8e9f03b1e417c7b93900420a3b2
Fatima <48693015+FatimaGarci@users.noreply.github.com>
2020-04-13 17:26:40 -0400

    Update script.js

--- Week-10/script.js
+++ Week-10/script.js
@@ -1,11 +1,12 @@
 // The information is store whenever we create any object, it's always created in the Heap space. 
 
-var me = new function (name) {
-  name = "Fatima Garcia"
+var me =  {
+  name :  "Fatima",
+  lastName: "Garcia"
 }
 console.log(me);
 
-let sayHi = (name) => {
-    console.log("Hello" + name);
+let sayHi = () => {
+    return "Hello" + this.name;
 }
-sayHi("Fatima");
+sayHi();
